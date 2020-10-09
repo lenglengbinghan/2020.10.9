068e7c2ed71a0e37c1653664941f2b667ead5fa2
lenglengbinghan <1109875584@qq.com>
2020-10-10 02:29:04 +0800

    first commit

---
+++ 2020.10.9/Assets/OtherSphere.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherSphere : MonoBehaviour
+{
+    public float friction;
+    public float mass;
+    public float radius;
+
+    [NonSerialized]
+    public Vector3 currentV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //摩擦力
+        Vector3 frictionDeltaV = -Time.deltaTime * friction * currentV.normalized;
+        //防止摩擦力反向运动
+        Vector3 finalV = currentV + frictionDeltaV;
+        if (finalV.x * currentV.x <= 0)
+            frictionDeltaV.x = -currentV.x;
+        if (finalV.y * currentV.y <= 0)
+            frictionDeltaV.y = -currentV.y;
+        if (finalV.z * currentV.z <= 0)
+            frictionDeltaV.z = -currentV.z;
+
+        //应用加速度
+        Vector3 curV = currentV + frictionDeltaV;
+        transform.Translate((curV + currentV) * Time.deltaTime / 2);
+        currentV = curV;
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("爱我别走");
+        if (collision.gameObject.name == "upwall" || collision.gameObject.name == "downwall")
+        {
+            currentV = Vector3.Reflect(currentV, Vector3.forward);
+        }
+        if (collision.gameObject.name == "leftwall" || collision.gameObject.name == "rightwall")
+        {
+            currentV = Vector3.Reflect(currentV, Vector3.right);
+        }
+
+    }
+    void Update()
+    {
+        
+    }
+}
